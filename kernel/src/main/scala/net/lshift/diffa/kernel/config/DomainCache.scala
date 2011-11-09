0c52d0fe6065df6fc429bab8e8637aeb9eef3ff0
Paul Jones <paulj@lshift.net>
2011-11-09 14:52:17 +1000

    [#361] Switch to using BoneCP.
    Note that this currently won't build due to slf4j conflicts in the Jetty classloader.

---
+++ kernel/src/main/scala/net/lshift/diffa/kernel/config/DomainCache.scala
@@ -0,0 +1,13 @@
+package net.lshift.diffa.kernel.config
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: paul
+ * Date: 09/11/2011
+ * Time: 14:00
+ * To change this template use File | Settings | File Templates.
+ */
+
+class DomainCache {
+
+}
